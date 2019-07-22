935caf9c1b317e151885dde4139c3066097727c6
Erick Kendall <ekendall@franklinamerican.com>
2019-07-22 08:20:30 -0500

    more dns

--- route.py
+++ route.py
@@ -7,11 +7,19 @@ for profile_name in profile_list:
   session = boto3.Session(profile_name=profile_name)
   client = session.client('route53', region_name='us-east-1')
 
-  for key, val in client.list_hosted_zones().items():
+  response = client.list_hosted_zones()
+  for key, val in response.items():
     if key == 'HostedZones':
-      for a in val:
-        x.append(a['Id']) 
-	
-for i in x:
-  resources = client.list_resource_record_sets(HostedZoneId=i)
-  print(resources)
+      for i in val:
+        print(f"\t{i['Name']}")
+        l = client.list_resource_record_sets(HostedZoneId=i['Id'])
+        for a,b in l.items():
+          if a == 'ResourceRecordSets':
+            for each in b:
+              for ky, vl in each.items():
+                if ky == 'Type':
+                  typ = vl
+                if ky == 'ResourceRecords':
+                  for everything in vl:
+                    #print(f"\t\t{everything}")
+                    print(f"\t\tType {typ} is {everything['Value']}")
