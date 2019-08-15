dacd1af02941a4a3a34e5d2104e5e8b2a87f004c
Erick Kendall <ekendall@franklinamerican.com>
2019-08-15 14:32:10 -0500

    timer function

---
+++ cschafer/tim.py
@@ -0,0 +1,16 @@
+def my_timer(orig_func):
+  import time
+
+  def wrapper(*args, **kwargs):
+    t1 = time.time()
+    result = orig_func(*args, **kwargs)
+    t2 = time.time() - t1
+    print('{} ran in: {} sec'.format(orig_func.__name__, t2))
+    return result
+  return wrapper
+
+@my_timer
+def display_info(name,age):
+  print('display_info ran with arguments ({}, {})'.format(name,age))
+
+display_info('Hank Hill', 30)
