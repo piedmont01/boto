fb9e79992457cfeb88db449ac387320a9d75415d
Erick Kendall <ekendall@franklinamerican.com>
2019-08-23 06:33:44 -0500

    more security groups

--- security_revisit.py
+++ security_revisit.py
@@ -1,24 +1,23 @@
 import boto3
 from botocore.exceptions import ClientError
 
-ec2 = boto3.client('ec2')
 
-try:
-    response = ec2.describe_security_groups()
-    values=response.values()
-except ClientError as e:
-    print(e)
+#try:
+#    response = ec2.describe_security_groups()
+#    values=response.values()
+#except ClientError as e:
+#    print(e)
 
 
 
 #print(response['SecurityGroups'])
 
-for item in values:
-  if isinstance(item, list):
-    for p in item:
-      if isinstance(p.values(), list):
-        for thing in p.values():
-          print(thing)
+#for item in values:
+#  if isinstance(item, list):
+#    for p in item:
+#      if isinstance(p.values(), list):
+#        for thing in p.values():
+#          print(thing)
 #i = 0
 #
 #sec_group_id_list = set()
@@ -28,3 +27,22 @@ for item in values:
 #  i=i+1
 #
 #print(sec_group_id_list)
+
+profile_list = [ '133', '202', '230', '272', '353', '439',] 
+
+for profile in profile_list:
+  session = boto3.Session(profile_name = profile)
+  ec2 = session.client('ec2', region_name = 'us-east-1')
+  security_group_list = []
+  print(f"Profile name {profile}")
+  for p in range(len(ec2.describe_security_groups().get('SecurityGroups'))):
+    security_group_list.append(ec2.describe_security_groups().get('SecurityGroups')[p].get('GroupId'))
+
+  for sec in security_group_list:
+    ec2 = session.resource('ec2' , region_name='us-east-1')
+    security_group = ec2.SecurityGroup(sec)
+
+#  for sec in security_group_list:
+#    ec2 = session.resource('ec2' , region_name='us-east-1')
+#    security_group = ec2.SecurityGroup(sec)
+#    print(security_group)
